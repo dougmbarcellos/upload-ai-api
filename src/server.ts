e291d3980a7b77c2b361c52fd54746ee3a9c5f91
Douglas M. Barcellos <douglasa321@gmail.com>
2023-09-12 22:15:12 -0300

    feat: add and implement routes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
+import fastifyCors from '@fastify/cors'
 import { fastify } from 'fastify'
+import { createTranscriptionRoute } from './routes/create-transcription'
+import { geneateAICompletionRoute as generateAICompletionRoute } from './routes/generate-ai-completion'
+import { getAllPromptsRoute } from './routes/get-all-prompts'
+import { uploadVideoRoute } from './routes/upload-video'
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'Hello world'
+app.register(fastifyCors, {
+  origin: '*'
 })
+
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
 
 app.listen({
   port: 3333
